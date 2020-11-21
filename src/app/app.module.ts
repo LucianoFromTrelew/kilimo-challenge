7a71062b3cef2509152c03229bda2aea074d1859
Luciano Serruya Aloisi <lucianoserruya@gmail.com>
2020-11-21 20:14:42 -0300

    Add toolbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
-import { MatSliderModule } from "@angular/material/slider";
+import { MatToolbarModule } from "@angular/material/toolbar";
 
 @NgModule({
   declarations: [AppComponent],
@@ -13,7 +13,7 @@ import { MatSliderModule } from "@angular/material/slider";
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatSliderModule
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
