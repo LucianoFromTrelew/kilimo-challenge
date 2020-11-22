7bfce3c59fcdb515b95311137b0fd1b0b10a50ae
Luciano Serruya Aloisi <lucianoserruya@gmail.com>
2020-11-22 16:27:13 -0300

    Add firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyAcUluSVHo_VW3kW-RzeQSX_x1FOps8vz0",
+    authDomain: "kilimo-challenge.firebaseapp.com",
+    databaseURL: "https://kilimo-challenge.firebaseio.com",
+    projectId: "kilimo-challenge",
+    storageBucket: "kilimo-challenge.appspot.com",
+    messagingSenderId: "215638972223",
+    appId: "1:215638972223:web:431e12f1620f7d5b38658a"
+  };
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyAcUluSVHo_VW3kW-RzeQSX_x1FOps8vz0",
+    authDomain: "kilimo-challenge.firebaseapp.com",
+    databaseURL: "https://kilimo-challenge.firebaseio.com",
+    projectId: "kilimo-challenge",
+    storageBucket: "kilimo-challenge.appspot.com",
+    messagingSenderId: "215638972223",
+    appId: "1:215638972223:web:431e12f1620f7d5b38658a"
+  }
 };
 
 /*
